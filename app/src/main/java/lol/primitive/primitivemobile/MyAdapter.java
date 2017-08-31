8efd9ce53b7c1aa388beb10df93c67e83996191f
Rahul Krishnan <emailrahulk64@gmail.com>
2017-08-30 20:53:10 -0600

    Add Zoom to Gallery using PhotoView

--- app/src/main/java/lol/primitive/primitivemobile/MyAdapter.java
+++ app/src/main/java/lol/primitive/primitivemobile/MyAdapter.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
