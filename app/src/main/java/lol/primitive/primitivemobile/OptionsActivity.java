b1cf680004d3febce95f52147d44ea3ceb68a70c
Rahul Krishnan <emailrahulk64@gmail.com>
2017-09-16 20:43:49 -0600

    Add Color Picker for Background

--- app/src/main/java/lol/primitive/primitivemobile/OptionsActivity.java
+++ app/src/main/java/lol/primitive/primitivemobile/OptionsActivity.java
@@ -1,15 +1,28 @@
 package lol.primitive.primitivemobile;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.shapes.Shape;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 public class OptionsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -46,6 +59,37 @@ public class OptionsActivity extends AppCompatActivity implements AdapterView.On
         sizesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sizesSpinner.setAdapter(sizesAdapter);
         sizesSpinner.setOnItemSelectedListener(this);
+
+        //Color Picker Initialization
+        final View colorPicker = (View) findViewById(R.id.colorPickerBtn);
+        colorPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ColorPickerDialogBuilder
+                        .with(OptionsActivity.this)
+                        .setTitle("Choose Background Color")
+                        .initialColor(Color.rgb(255, 255, 255))
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .density(12)
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+                                Toast.makeText(OptionsActivity.this, "onColorSelected: 0x" + Integer.toHexString(selectedColor), Toast.LENGTH_LONG);
+                            }
+                        })
+                        .setPositiveButton("OK", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                colorPicker.setBackgroundColor(selectedColor);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).build().show();
+            }
+        });
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
